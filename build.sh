f008a6fb7a27d095803faaab95d33f589e04502f
Barry van Veen <barry+github@barryvanveen.nl>
2025-11-09 17:15:35 +0100

    WIP 4

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 # !/bin/bash
 
+# scrape latest observations
+./getObservations.sh
+
+# output hugo version
 hugo version
 
+# build with production base URL
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   hugo -b $BASE_URL
 
+# build branch for CI/CD preview with Cloudflare base URL
 else
   hugo -b $CF_PAGES_URL
 
