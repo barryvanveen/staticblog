081f3b82b427e83709ffb1f0f84e95232e5827b1
Barry van Veen <barry+github@barryvanveen.nl>
2025-11-09 17:30:56 +0100

    WIP 5

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 # !/bin/bash
 
 # scrape latest observations
+echo "## Getting observations..."
 ./getObservations.sh
+echo "## Finished getting observations:"
+cat assets/observations.json
 
 # output hugo version
+echo "## Hugo version"
 hugo version
 
+echo "## Hugo build"
 # build with production base URL
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   hugo -b $BASE_URL
