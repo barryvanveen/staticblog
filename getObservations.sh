67495e8756ac6e10b62071435007c1202bdc8c55
Barry van Veen <barry+github@barryvanveen.nl>
2025-11-09 17:13:52 +0100

    WIP 3

--- getObservations.sh
+++ getObservations.sh
@@ -1,28 +1,24 @@
 # !/bin/bash
 
+## Dependencies ##
+# curl, xmllint (libxml2)
+
+## What this script does ##
+
 # download the contents of https://waarneming.nl/users/408111/observations/?advanced=on
-
 # find the table with selector "div.app-content-section table"
-
 # take the first 3 rows where "td.column-species" does not contain "i.status-uncertain" and where "td::last" contains "i.fa-camera"
-
 # iterate over the 3 rows
-
   # collect the href from "td::first a" and download the page
-
   # from that page, take the following information
-    # common name: .app-content h1 span.species-common-name
-    # scientific name: .app-content div.species-subtitle i.species-scientific-name
+    # common name: .app-content .species-common-name
+    # scientific name: .app-content .species-scientific-name
     # photo: .app-content div#photos img
-
   # convert the first photo to a base64 encoded string
+# output the url, common name, scientific name and base64 encoded photo to a json file called observations.json
 
-  # output the url, common name, scientific name and base64 encoded photo to a json file called observations.json
-
-#!/bin/bash
 set -euo pipefail
 
-# Minimal deps: curl, xmllint (libxml2)
 PAGE_URL="https://waarneming.nl/users/408111/observations/?advanced=on"
 BASE_URL="https://waarneming.nl"
 
@@ -32,7 +28,7 @@ tmpimg=$(mktemp)
 trap 'rm -f "$tmp" "$tmp2" "$tmpimg"' EXIT
 
 # fetch main page
-curl -sL "$PAGE_URL" -o "$tmp"
+curl -sL -b "django_language=en" "$PAGE_URL" -o "$tmp"
 
 COND="td[contains(concat(' ',normalize-space(@class),' '),' column-species ')][not(.//i[contains(concat(' ',normalize-space(@class),' '),' status-uncertain ')])] and td[last()]//i[contains(concat(' ',normalize-space(@class),' '),' fa-camera ')]"
 ROWS_XPATH="//div[contains(concat(' ',normalize-space(@class),' '),' app-content-section ')]//table//tr[${COND}]"
@@ -59,19 +55,12 @@ json_escape() {
 items=()
 for i in $(seq 1 $max); do
   href=$(xmlexpr "string(${ROWS_XPATH}[position()=$i]/td[1]//a/@href)" "$tmp")
-  href=$(echo "$href" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
-  if [ -z "$href" ]; then continue; fi
-  if [[ "$href" =~ ^http ]]; then
-    url="$href"
-  else
-    [[ "$href" != /* ]] && href="/$href"
-    url="${BASE_URL}${href}"
-  fi
+  url="${BASE_URL}${href}"
 
-  curl -sL "$url" -o "$tmp2"
+  curl -sL -b "django_language=en" "$url" -o "$tmp2"
 
-  common_xpath="string(//div[contains(concat(' ',normalize-space(@class),' '),' app-content ')]//h1//span[contains(concat(' ',normalize-space(@class),' '),' species-common-name ')])"
-  sci_xpath="string(//div[contains(concat(' ',normalize-space(@class),' '),' app-content ')]//div[contains(concat(' ',normalize-space(@class),' '),' species-subtitle ')]//i[contains(concat(' ',normalize-space(@class),' '),' species-scientific-name ')])"
+  common_xpath="string((//div[contains(@class,'app-content')]//span[contains(@class,'species-common-name')])[1])"
+  sci_xpath="string((//div[contains(@class,'app-content')]//i[contains(@class,'species-scientific-name')])[1])"
   photo_xpath="string(//div[contains(concat(' ',normalize-space(@class),' '),' app-content ')]//div[@id='photos']//img[1]/@src)"
 
   common=$(xmlexpr "$common_xpath" "$tmp2" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
